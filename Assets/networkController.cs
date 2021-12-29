f178a575df26fe1d072093e0e1621cf010c862c7
Rashaad Cassim <rashcassim@gmail.com>
2021-12-29 14:36:53 +0200

    Added basic network skeleton
    Added skeleton to allow post requets to be made to an API from a central
    network controller

--- Assets/networkController.cs
+++ Assets/networkController.cs
@@ -1,25 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class networkController : MonoBehaviour
 {
-    private float refresh;
+    private float refreshRate;
+
+    private string serverResponse;
 
     private void updateFlashing()
     {
         GameObject visualEntrainmentObject = GameObject.Find("visualEntrainment");
         visualEtrainment visualEtrainmentsScript = (visualEtrainment)visualEntrainmentObject.GetComponent(typeof(visualEtrainment));
-        visualEtrainmentsScript.setRefresh(refresh);
-        refresh+=1.0f;
+        visualEtrainmentsScript.setRefresh(refreshRate);
+    }
+
+    // Updates settings of current entrainment if they are due to be changed
+    private void updateEntrainment()
+    {
+        // Upodate refreshRate
+        // Update audio frequnecy
+    }
+
+    IEnumerator makeRequest()
+    {
+        // Populate form with fields to make request to API
+        WWWForm form = new WWWForm();
+        form.AddField("myField", "myData");
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://www.google.com/index.html", form)) // Pull from a common file
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                serverResponse = www.downloadHandler.text;
+                Debug.Log(www.downloadHandler.text);
+                Debug.Log("Form upload complete!");
+            }
+        }
+    }
+
+    // Querries settings from API on AWS every 30 s
+    private async void querrySettings()
+    {
+        StartCoroutine(makeRequest()); // Allows the coroutine to be non blocking
     }
     void Start()
     {
-        refresh = 2.0f;
-        InvokeRepeating("updateFlashing", 10.0f, 10.0f);
-    }
+        refreshRate = 2.0f; // Pull in from a common file for all settings
+        InvokeRepeating("querrySettings", 10.0f, 30.0f);
+}
 
-    void Update()
+void Update()
     {
         
     }
